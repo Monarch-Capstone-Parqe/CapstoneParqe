0584b3448f62924e69f77b58b828cfb7a784491b
Alan Schaaf <alanschaaf@Alans-MacBook-Pro.local>
2023-11-02 19:48:48 -0700

    implemented a hack to allow to download uploaded file to downloads folder of users file system

--- userAPI/index.js
+++ userAPI/index.js
@@ -7,7 +7,24 @@ function showSubmittedFile() {
     console.log("File Submitted");
     console.log("Name: " + fileInput.files.item(0).name);
     console.log("Size: " + fileInput.files.item(0).size + " bytes");
+
+    downloadFile(fileInput);
   } else {
     console.log("No File Selected");
   }
+}
+
+function downloadFile(fileInput) {
+  // hidden anchor element that allows to "click" a href
+  let a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+
+  let blob = new Blob([fileInput]);
+  let url = URL.createObjectURL(blob);
+
+  a.href = url;
+  a.download = fileInput.files.item(0).name;
+  a.click();
+  window.URL.revokeObjectURL(url);
 }

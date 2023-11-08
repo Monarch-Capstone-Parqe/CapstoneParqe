1d1029d0482a8d6d6a14617be092c68b744f4932
aschaaf1 <114626901+aschaaf1@users.noreply.github.com>
2023-11-08 08:07:35 -0800

    PARQE-5 (#3)
    * javascript code that adds an event lister to submit button clicks and console logs file submittion info when clicked
    
    * implemented a hack to allow to download uploaded file to downloads folder of users file system
    
    * added more style to page: psu logo, psu fonts, etc
    
    * removed image not being used
    
    * minor formatting
    
    * updated rgb values to use offical psu brand colors
    
    * added useful comments
    
    * added staffAPI directory
    
    * added fonts and image folders to staffAPI dir
    
    * rename file
    
    * rename file
    
    * updated readme
    
    ---------
    
    Co-authored-by: Alan Schaaf <alanschaaf@Alans-MacBook-Pro.local>

---
+++ userAPI/index.js
@@ -0,0 +1,30 @@
+let submitButton = document.getElementById("submit-button");
+submitButton.addEventListener("click", showSubmittedFile);
+
+function showSubmittedFile() {
+  let fileInput = document.getElementById("file-input");
+  if (fileInput.files.item(0) != null) {
+    console.log("File Submitted");
+    console.log("Name: " + fileInput.files.item(0).name);
+    console.log("Size: " + fileInput.files.item(0).size + " bytes");
+
+    downloadFile(fileInput);
+  } else {
+    console.log("No File Selected");
+  }
+}
+
+function downloadFile(fileInput) {
+  // hidden anchor element that allows to "click" a href
+  let a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+
+  let blob = new Blob([fileInput]);
+  let url = URL.createObjectURL(blob);
+
+  a.href = url;
+  a.download = fileInput.files.item(0).name;
+  a.click();
+  window.URL.revokeObjectURL(url);
+}

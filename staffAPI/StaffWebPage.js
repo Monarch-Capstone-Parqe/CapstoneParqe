19e5bea852cfdfcae2c8654ac025738efa1ce3dd
Jordan Hyde <jordhyde@pdx.edu>
2023-11-13 18:41:25 -0800

    Putting up a skeleton for desired webpage items, still pretty rough

---
+++ staffAPI/StaffWebPage.js
@@ -0,0 +1,26 @@
+
+
+//todo create job objects
+//
+function approve()
+{
+//update status of job
+//send job to printers
+console.log("approve")
+}
+
+function deny()
+{
+//update status of job
+//remove from view
+console.log("deny")
+}
+
+function refreshJobs()
+{
+//get new jobs from database
+//create job objects
+//populate sections with new data, connected to objects
+//remove jobs that have been updated already
+console.log("refresh")
+}

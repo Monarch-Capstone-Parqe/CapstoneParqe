c630400ee1e0b39e6f68be2d11b5fe0f938e23e1
aschaaf1 <114626901+aschaaf1@users.noreply.github.com>
2023-11-21 16:03:42 -0800

    PARQE- 35 (#5)
    * added user end success/fail confirmation of file submission
    
    * changed all id to classes
    
    ---------
    
    Co-authored-by: Alan Schaaf <alanschaaf@Alans-MacBook-Pro.local>

--- static/index.js
+++ static/index.js
@@ -1,14 +1,15 @@
-let submitButton = document.querySelector("#submit-button");
+let submitButton = document.querySelector(".submit-button");
 submitButton.onclick = function (event) {
   event.preventDefault();
   uploadAndShowFile();
 };
 function uploadAndShowFile() {
-  const fileInput = document.querySelector("#file-input");
+  const fileInput = document.querySelector(".file-input");
   const file = fileInput.files[0];
 
   if (!file) {
     console.error("No file selected.");
+    displayUploadStatus("noFile");
     return;
   }
 
@@ -24,24 +25,15 @@ function uploadAndShowFile() {
       console.log(data);
       // Handle the response data as needed
       showFileInfo(fileInput);
+      displayUploadStatus("success");
     })
     .catch((error) => {
       console.error("Error:", error);
+      displayUploadStatus("failure");
     });
 }
 
-function showFileInfo(fileInput) {
-  if (fileInput.files.item(0) != null) {
-    console.log("File Submitted");
-    console.log("Name: " + fileInput.files.item(0).name);
-    console.log("Size: " + fileInput.files.item(0).size + " bytes");
-
-    // Additional logic to display or handle the file info as needed
-  } else {
-    console.log("No File Selected");
-  }
-}
-
+// downloads the inputted file into user local directory
 function downloadFile(fileInput) {
   // hidden anchor element that allows to "click" a href
   let a = document.createElement("a");
@@ -55,4 +47,43 @@ function downloadFile(fileInput) {
   a.download = fileInput.files.item(0).name;
   a.click();
   window.URL.revokeObjectURL(url);
+}
+
+// displays file info to console for debugging purposes
+function showFileInfo(fileInput) {
+  if (fileInput.files.item(0) != null) {
+    console.log("File Submitted");
+    console.log("Name: " + fileInput.files.item(0).name);
+    console.log("Size: " + fileInput.files.item(0).size + " bytes");
+
+    // Additional logic to display or handle the file info as needed
+  } else {
+    console.log("No File Selected");
+  }
+}
+
+// notifies the user of web api of status of their file upload in the html form
+function displayUploadStatus(status) {
+  const statusMessageContainer = document.querySelector(".status-message-container");
+  statusMessageContainer.innerHTML = '';
+  document.querySelector(".upload-form").reset();
+
+  if (status === "success") {
+    insertGoogleIcon(statusMessageContainer, "check", "lime");
+  } else if (status === "failure"){
+    insertGoogleIcon(statusMessageContainer, "close", "red");
+  } else {
+    insertGoogleIcon(statusMessageContainer, "question_mark", "yellow");
+  }
+
+  // status after a given amount of milliseconds
+  setTimeout(function() {
+    statusMessageContainer.innerHTML = '';
+  }, 2000);
+}
+
+// inserts a google icon into an element. Input parameters for element, icon name, and desired color
+function insertGoogleIcon(element, iconName, color) {
+  element.style.color = color;
+  element.innerHTML = '<span class="material-symbols-outlined">'+iconName+'</span>';
 }

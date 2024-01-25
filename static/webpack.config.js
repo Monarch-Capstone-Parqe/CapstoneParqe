01bb1bfdecafdef526d29ece4cda00a5c7c27eff
Jordan Hyde <jordhyde@pdx.edu>
2024-01-24 22:47:31 -0800

    webpack change, comments for readability

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js',
+    filename: 'staff.bundle.js',
   },
   resolve: {
         extensions: ['.ts', '.tsx', '.js', '.json']

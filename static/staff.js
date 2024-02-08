934601ace6271a0a2c65c23011d5f508791d0550
Jordan Hyde <jordhyde@pdx.edu>
2024-02-08 14:03:47 -0800

    Made everything look nicer, adjusted size of window. added build plate grid

--- static/staff.js
+++ static/staff.js
@@ -495,5 +495,35 @@ function insertDeniedTableRow(order) {
     deniedCell.classList.add('table-data');
 }
 
+
+
+function openPreview(gcode)
+{
+    const previewModal = document.querySelector('.gcode-preview-modal');
+    const closeButton = document.getElementById('preview-close-button');
+    previewModal.style.display = 'block';
+    //gcode-preview canvas
+    let gcodePrev = document.getElementById('preview-canvas');
+    gcodePrev.id = "preview-canvas"
+    //Process the gcode after the canvas is initialized
+    const preview = GCodePreview.init({
+      canvas: gcodePrev,
+      buildVolume: { x: 300, y: 300, z: 0 },
+      drawBuildVolume: { x: 300, y: 300, z: 0 },
+      initialCameraPosition: [90, 75, 150],
+      renderExtrusion: false,
+      renderTravel: false,
+      renderTubes: false,
+      extrusionColor: 'hotpink',
+      backgroundColor: '#eee',
+      travelColor: new THREE.Color('lime')
+    });
+
+    preview.processGCode(gcode);
+    closeButton.onclick = function() {
+        previewModal.style.display = 'none';
+    }
+}
+
 //Interval refreshing orders from database continuously to keep the page up to date
 let intervalId = setInterval(refreshOrdersWrapper, 10000);

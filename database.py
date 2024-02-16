56984eed116833422bb77b1adb3cf5cab9f5c66b
Alan Schaaf <alanschaaf@Alans-MacBook-Pro.local>
2024-02-16 09:28:32 -0800

    changed order of 'order' parameters to match that or form submission

--- database.py
+++ database.py
@@ -39,15 +39,15 @@ def create_tables():
         conn.execute(text("CREATE TABLE IF NOT EXISTS orders("
                             "id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,"
                             "email VARCHAR NOT NULL,"
-                            "layer_height VARCHAR NOT NULL,"      
+                            "filament_type VARCHAR NOT NULL,"
                             "nozzle_size VARCHAR NOT NULL,"      
+                            "layer_height VARCHAR NOT NULL,"      
                             "infill NUMERIC(3,2) NOT NULL,"          
                             "quantity INTEGER NOT NULL," 
                             "note VARCHAR,"
                             "gcode_path VARCHAR NOT NULL,"
                             "prusa_output VARCHAR NOT NULL,"
                             "price NUMERIC(6,2) NOT NULL,"
-                            "filament_type VARCHAR NOT NULL,"
                             "date DATE DEFAULT CURRENT_DATE)"))
 
         # Create the approved_orders table with a foreign key reference to staff table
@@ -73,26 +73,26 @@ def insert_order(email, layer_height=None, nozzle_size=None, infill=None, quanti
 
     Parameters:
         email (str): The email of the customer placing the order.
-        layer_height (str): The layer height of the 3D print.
+        filament_type (str): The material and color of the filament
         nozzle_size (str): The nozzle size of the 3D print.
+        layer_height (str): The layer height of the 3D print.
         infill (float): The infill percentage of the 3D print.
         quantity (int): The quantity of items ordered.
         note (str): Any additional notes or comments.
         prusa_output (str): The PrusaSlicer output file path.
         gcode_path (str): The G-code file path.
         price (float): The total price of the order.
-        filament_type (str): The material and color of the filament
 
     Returns:
         int: The ID of the inserted order.
     """
      
     with engine.begin() as conn:
-        result = conn.execute(text("INSERT INTO orders(email, layer_height, nozzle_size, infill, quantity, note, prusa_output, gcode_path, price, filament_type, date) "
-                          "VALUES (:email, :layer_height, :nozzle_size, :infill, :quantity, :note, :prusa_output, :gcode_path, :price, :filament_type, :date) RETURNING id"),
-                     {"email": email, "layer_height": layer_height, "nozzle_size": nozzle_size,
+        result = conn.execute(text("INSERT INTO orders(email, filament_type, nozzle_size, layer_height, infill, quantity, note, prusa_output, gcode_path, price, date) "
+                          "VALUES (:email, :filament_type, :nozzle_size, :layer_height, :infill, :quantity, :note, :prusa_output, :gcode_path, :price, :date) RETURNING id"),
+                     {"email": email, "filament_type": filament_type, "nozzle_size": nozzle_size, "layer_height": layer_height,
                       "infill": infill, "quantity": quantity, "note": note,
-                      "prusa_output": prusa_output, "gcode_path": gcode_path, "price": price, "filament_type": filament_type, "date": date.today()})
+                      "prusa_output": prusa_output, "gcode_path": gcode_path, "price": price, "date": date.today()})
         
         order_id = result.fetchone()["id"]
         conn.execute(text("INSERT INTO pending_orders(order_id) VALUES (:order_id)"), {"order_id": order_id})
